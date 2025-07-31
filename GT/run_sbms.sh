067358b9c6913087a71bef788c83b9331ac5b7af
Kushal Bose <kushalbose92@gmail.com>
2025-07-31 13:39:48 +0530

    Add files via upload

---
+++ GT/run_sbms.sh
@@ -0,0 +1,62 @@
+# python main_SBMs_node_classification.py --config 'PATTERN_LapPE_BN.json' --seed 41 --c 0.0 | tee output/PATTERN/pattern1.txt
+
+# python main_SBMs_node_classification.py --config 'PATTERN_LapPE_BN.json' --seed 41 --c 0.0001 | tee output/PATTERN/pattern2.txt
+
+# python main_SBMs_node_classification.py --config 'PATTERN_LapPE_BN.json' --seed 41 --c 1.0 | tee output/PATTERN/pattern3.txt
+
+# python main_SBMs_node_classification.py --config 'PATTERN_LapPE_BN.json' --seed 41 --c 10.0 | tee output/PATTERN/pattern4.txt
+
+
+# python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_BN.json' --seed 41 --c 0.0 | tee output/CLUSTER/cluster1.txt
+
+# python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_BN.json' --seed 41 --c 0.0001 | tee output/CLUSTER/cluster2.txt
+
+# python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_BN.json' --seed 41 --c 1.0 | tee output/CLUSTER/cluster3.txt
+
+# python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_BN.json' --seed 41 --c 10.0 | tee output/CLUSTER/cluster4.txt
+
+
+
+
+# python main_SBMs_node_classification.py --config 'PATTERN_LapPE_BN.json' --seed 41 --full_graph 'True' --ctgt_flag 'False' | tee output/PATTERN/pattern2.txt
+# python main_SBMs_node_classification.py --config 'PATTERN_LapPE_BN.json' --seed 41 --full_graph 'False' --ctgt_flag 'True' | tee output/PATTERN/pattern3.txt
+# python main_SBMs_node_classification.py --config 'PATTERN_LapPE_BN.json' --seed 41 --full_graph 'True' --ctgt_flag 'True' | tee output/PATTERN/pattern4.txt
+
+
+
+# python main_SBMs_node_classification.py --config 'PATTERN_LapPE_LN.json' --seed 41 --full_graph 'False' --ctgt_flag 'False' | tee output/PATTERN/pattern5.txt
+# python main_SBMs_node_classification.py --config 'PATTERN_LapPE_LN.json' --seed 41 --full_graph 'True' --ctgt_flag 'False' | tee output/PATTERN/pattern6.txt
+# python main_SBMs_node_classification.py --config 'PATTERN_LapPE_LN.json' --seed 41 --full_graph 'False' --ctgt_flag 'True' | tee output/PATTERN/pattern7.txt
+# python main_SBMs_node_classification.py --config 'PATTERN_LapPE_LN.json' --seed 41 --full_graph 'True' --ctgt_flag 'True' | tee output/PATTERN/pattern8.txt
+
+
+
+
+# =========================
+
+# python main_SBMs_node_classification.py --config 'PATTERN_RWPE_BN.json' | tee pattern3.txt
+# python main_SBMs_node_classification.py --config 'PATTERN_RWPE_LN.json' | tee pattern4.txt
+
+
+
+# python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_BN.json' --seed 41 --full_graph 'False' --ctgt_flag 'False' | tee output/CLUSTER/cluster1.txt
+# python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_BN.json' --seed 41 --full_graph 'True' --ctgt_flag 'False' | tee output/CLUSTER/cluster2.txt
+# python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_BN.json' --seed 41 --full_graph 'False' --ctgt_flag 'True' | tee output/CLUSTER/cluster3.txt
+# python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_BN.json' --seed 41 --full_graph 'True' --ctgt_flag 'True' | tee output/CLUSTER/cluster4.txt
+
+
+
+# python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_LN.json' --seed 41 --full_graph 'False' --ctgt_flag 'False' | tee output/CLUSTER/cluster5.txt
+# python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_LN.json' --seed 41 --full_graph 'True' --ctgt_flag 'False' | tee output/CLUSTER/cluster6.txt
+# python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_LN.json' --seed 41 --full_graph 'False' --ctgt_flag 'True' | tee output/CLUSTER/cluster7.txt
+# python main_SBMs_node_classification.py --config 'CLUSTER_LapPE_LN.json' --seed 41 --full_graph 'True' --ctgt_flag 'True' | tee output/CLUSTER/cluster8.txt
+
+
+
+
+
+
+
+# ============================
+# python main_SBMs_node_classification.py --config 'CLUSTER_RWPE_BN.json' | tee cluster3.txt
+# python main_SBMs_node_classification.py --config 'CLUSTER_RWPE_LN.json' | tee cluster4.txt
